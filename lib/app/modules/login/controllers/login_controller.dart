61004f4f8f0066c7106b6f5fde55594a269fd9e9
aditgocendra <gocendra123@gmail.com>
2022-09-06 13:36:59 +0700

    feature login

---
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -0,0 +1,83 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:kelvin_project/app/routes/app_pages.dart';
+
+import 'package:kelvin_project/services/firebase/firestore.service.dart';
+import 'package:kelvin_project/services/local/encryption.dart';
+
+class LoginController extends GetxController {
+  // Loading
+  final isLoading = false.obs;
+
+  // Error Message
+  final errorMessageForm = ''.obs;
+
+  // Editing Controller
+  TextEditingController emailCtl = TextEditingController();
+  TextEditingController passCtl = TextEditingController();
+
+  // Login
+  Future login() async {
+    // Validation Form
+    if (!validationForm()) {
+      errorMessageForm.value = 'Beberapa form terlihat kosong';
+      return;
+    }
+
+    String emailInput = emailCtl.text.trim();
+    String passInput = passCtl.text.trim();
+
+    // Check Email Format
+    if (!GetUtils.isEmail(emailInput)) {
+      errorMessageForm.value = 'Format email salah';
+      return;
+    }
+
+    // Get user account
+    isLoading.toggle();
+    final resultUser = await getUserAccount(emailInput);
+
+    // Check user account available or not
+    if (resultUser.docs.isEmpty) {
+      errorMessageForm.value = 'Pengguna tidak ditemukan';
+      isLoading.toggle();
+      return;
+    }
+
+    // Decryption password from firebase
+    final userData = resultUser.docs[0];
+    final passDec = EncryptionService.decryptAES(userData['password']);
+
+    // Check Password Same or not pass from firebase
+    if (passInput != passDec) {
+      errorMessageForm.value = 'Password salah';
+      isLoading.toggle();
+      return;
+    }
+
+    // Login Success
+    Get.offAndToNamed(Routes.HOME);
+  }
+
+  // GetUser Account
+  Future<QuerySnapshot> getUserAccount(String emailAddress) async {
+    return await FirestoreService.refUsers
+        .where('email', isEqualTo: emailAddress)
+        .limit(1)
+        .get();
+  }
+
+  // Validation Form
+  bool validationForm() {
+    if (emailCtl.text.isEmpty) {
+      return false;
+    }
+
+    if (passCtl.text.isEmpty) {
+      return false;
+    }
+
+    return true;
+  }
+}
---
+++ lib/app/modules/login/views/login_view.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:kelvin_project/app/globals/constant.dart';
+import 'package:kelvin_project/app/globals/styles.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+
+import '../controllers/login_controller.dart';
+
+class LoginView extends GetView<LoginController> {
+  const LoginView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+              width: 500,
+              margin: const EdgeInsets.all(32),
+              padding: const EdgeInsets.all(32),
+              decoration: BoxDecoration(
+                border: Border.all(color: primaryColor),
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(32),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Image.asset(
+                    'assets/images/logo/KLABELS_Logo_Front_Color_16-9.png',
+                    height: 120,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: TextField(
+                      controller: controller.emailCtl,
+                      style: const TextStyle(fontSize: 14),
+                      cursorColor: primaryColor,
+                      decoration: GlobalStyles.formInputDecoration('Email'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: TextField(
+                      controller: controller.passCtl,
+                      style: const TextStyle(fontSize: 14),
+                      cursorColor: primaryColor,
+                      obscureText: true,
+                      decoration:
+                          GlobalStyles.formInputDecoration('Kata Sandi'),
+                    ),
+                  ),
+                  Obx(
+                    () => Text(
+                      controller.errorMessageForm.value,
+                      style: const TextStyle(
+                        fontSize: 12,
+                        color: Colors.redAccent,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Obx(
+                    () {
+                      if (controller.isLoading.value) {
+                        return LoadingAnimationWidget.dotsTriangle(
+                            color: primaryColor, size: 50);
+                      }
+
+                      return ElevatedButton(
+                        onPressed: () {
+                          controller.login();
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: primaryColor,
+                          elevation: 0.5,
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          ),
+                          minimumSize: const Size.fromHeight(50),
+                        ),
+                        child: const Text(
+                          'Masuk',
+                          style: TextStyle(fontSize: 14),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
