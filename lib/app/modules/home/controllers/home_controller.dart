1219e3a51f5d72f9d66646f6a75ca3c18d301b47
aditgocendra <gocendra123@gmail.com>
2022-09-06 20:49:26 +0700

    add shared pref

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,6 +1,11 @@
 import 'package:get/get.dart';
+import 'package:kelvin_project/app/routes/app_pages.dart';
+import 'package:kelvin_project/services/local/shared_pref.dart';
 
 class HomeController extends GetxController {
+  final SharedPrefService pref = SharedPrefService();
+  final username = ''.obs;
+
   // Logged in
   final isLogin = false.obs;
 
@@ -8,12 +13,14 @@ class HomeController extends GetxController {
   final indexContent = 0.obs;
 
   void logout() {
-    isLogin.toggle();
+    pref.removeCache();
+    Get.offAndToNamed(Routes.LOGIN);
   }
 
   @override
-  void onInit() {
-    print('init home');
+  void onInit() async {
+    username.value = await pref.readCache();
+    print(username);
     super.onInit();
   }
 }
